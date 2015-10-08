16ec49097e106215164a7f90265b472c903fcafa
nelsonchung <chihchun.chung@gmail.com>
2015-10-08 20:40:59 +0800

    Correct the informaiton.

--- sync_baidu.sh
+++ sync_baidu.sh
@@ -4,7 +4,7 @@
 
 #You need to install bypy.py by following command
 #1. git clone https://github.com/houtianze/bypy.git
-#2. sudo cp bypy.git /usr/bin/
+#2. sudo cp bypy.py /usr/bin/
 
 cd baidu/
 #You need to input the key when you first time to run bypy.py
