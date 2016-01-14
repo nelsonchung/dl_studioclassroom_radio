352aa94824d3424a56f9813f99080efe8cb31fe5
nelsonchung <chihchun.chung@gmail.com>
2016-01-14 20:21:36 +0800

    We need bypy.sh to sync mp3 to baidu.

--- setupenv.sh
+++ setupenv.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-sudo apt-get install curl dos2unix mimms mplayer lame id3v2
+sudo apt-get install curl dos2unix mimms mplayer lame id3v2 git
 sudo pip install -U eyeD3
+
+mkdir -p bypy
+cd bypy
+git clone https://github.com/houtianze/bypy.git
+cd bypy
+sudo cp bypy.py /usr/bin/
+cd -
+
+PWD=`pwd`
+echo "export PATH=\$\"PATH:$PWD\"" >> /home/nelson/.bashrc
