ce06a086dea23e160bc0f673c878fde857eb522f
nelsonchung <chihchun.chung@gmail.com>
2016-11-09 15:40:09 +0800

    for pip package

--- setupenv.sh
+++ setupenv.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 sudo apt-get install curl dos2unix mimms mplayer lame id3v2 git
 sudo apt-get install python-setuptools
-sudo easy_install pip
+#for pip
+curl http://python-distribute.org/distribute_setup.py | python
+curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py | python
+#sudo easy_install pip
 sudo pip install -U eyeD3
 
 git clone https://github.com/houtianze/bypy.git
