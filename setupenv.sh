99ab2559bb8b7c3abc9326cddff1702559555ac7
nelsonchung <chihchun.chung@gmail.com>
2016-11-09 15:06:42 +0800

    add some package for first time to use.

--- setupenv.sh
+++ setupenv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 sudo apt-get install curl dos2unix mimms mplayer lame id3v2 git
+sudo apt-get install python-setuptools
+sudo easy_install pip
 sudo pip install -U eyeD3
 
 git clone https://github.com/houtianze/bypy.git
